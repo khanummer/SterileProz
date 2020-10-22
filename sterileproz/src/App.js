96d2139c9dd537df739db4a0a2fc88f3b6ce648a
Ummer Khan <khanummergit@gmail.com>
2020-10-22 15:53:29 -0700

    Adds React-Router to App.js and index.js

--- sterileproz/src/App.js
+++ sterileproz/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Switch, Route, withRouter } from 'react-router-dom'
 import './App.css';
 
 function App() {
