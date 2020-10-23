d6e125eb9dcb1ed47bc0ac3deba2eb01f13aeb4c
Ummer Khan <khanummergit@gmail.com>
2020-10-23 05:22:48 -0700

    Adds AboutUs.js, ContactUs.js, Home.js, Edits App.js with Switch and Routes

--- sterileproz/src/App.js
+++ sterileproz/src/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom'
 import './App.css';
+import Home from './Home/Home';
+import AboutUs from './AboutUs/AboutUs';
+import ContactUs from './ContactUs/ContactUs';
 
-function App() {
-  return (
-    <div>
-      <div>hi</div>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div>
+//       <div>hi</div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+class App extends Component {
+
+  render(){
+    return(
+      <div>
+        <Switch>
+          <Route exact path="/" component={() => <Home/>}/>
+          <Route exact path="/contact" component={() => <ContactUs/>}/>
+          <Route exact path="/aboutus" component={() => <AboutUs/>}/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withRouter(App);
