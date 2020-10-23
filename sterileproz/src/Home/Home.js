081a3f8bfc7559026ba8dfba3907a63b259a6e97
Ummer Khan <khanummergit@gmail.com>
2020-10-23 05:45:43 -0700

    Adds multiple div's with content to Home.js

--- sterileproz/src/Home/Home.js
+++ sterileproz/src/Home/Home.js
@@ -4,7 +4,73 @@ import './Home.css';
 class Home extends Component {
     render() {
         return(
-            <div>hi</div>
+            <div>
+                <div>GET HELP 24 HOUR SERVICE</div>
+
+                <div>*number* CALL NOW</div>
+                <div>REQUEST HELP ONLINE HELP</div>
+                <div>FREE CONSULTATION CLICK HERE</div>
+
+                <div>
+                    Residental Covid-19 Sanitation, *img*
+                    Cleaning Sanitation Virus Elimination, *img*
+                    Commercial Covid-19 Sanitation, *img*
+                    Office Covid-19 Sanitaion, *img*
+                    Industrial Covid-19 Sanitation *img*
+                </div>
+
+                <div>
+                    CDC APPROVED
+                    "CDC Medical Cleaning Approved Methods for Hospitals Used"
+                </div>
+
+                <div>SterileProz Offers Many Kinds of Services We Sterilize:</div>
+                <div>
+                    - Residental Areas and Apartments
+                    - Buisness and Public Apartments
+                    - Nursing Homes
+                    - Warehouses
+                    - Schools and Universities
+                    - Daycares
+                    - Fitness Centers and Gyms
+                    - Studios
+                    - Automobiles
+                    - Libraries
+                    - Restaurants
+                    - Commercial Buildings
+                    - Grocery Stores
+                    
+                    And Much More!
+                </div>
+                <button>CALL TODAY FOR A FREE QUOTE!</button>
+
+                <div>To get a free quote or to learn more, fill out the form below</div>
+                <div>
+                    *form*
+                    First Name
+                    Last Name
+                    Email
+                    Phone Number
+                    Message
+                    Submit Message
+                </div>
+
+                <div>COVID-19 CLEANING</div>
+                <div>COVID-19 Sanitation Homes, Offices, Schools, Commercial</div>
+                
+                <div>TRAINING AND PROFESSIONAL SERVICES</div>
+                
+                <div>Highly Trained Specialists</div>
+                <div>
+                    Founded By A Doctor, Our Cleaners are Fully Trained In Hospital Level Cleaning Techniques,
+                    Guaranteed To Be The Best In The Industry, We Strive To Ensure Your Home is Sanitized 
+                    And Covid Free! 
+                </div>
+
+
+
+            </div>
+
         )
     }
 }
