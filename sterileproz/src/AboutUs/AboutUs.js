d49fbe57ab1d52fedbed6d35f6485f844f63ff0c
Ummer Khan <khanummergit@gmail.com>
2020-10-23 05:56:48 -0700

    Edits ContactUs and AboutUs to render on each Path

---
+++ sterileproz/src/AboutUs/AboutUs.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './AboutUs.css';
+
+class AboutUs extends Component {
+    render() {
+        return(
+            <div>
+
+                <div>About Us</div>
+
+            </div>
+
+        )
+    }
+}
+
+export default AboutUs;
---
+++ sterileproz/src/ContactUs/ContactUs.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './ContactUs.css';
+
+class ContactUs extends Component {
+    render() {
+        return(
+            <div>
+
+                <div>Contact Us</div>
+
+            </div>
+
+        )
+    }
+}
+
+export default ContactUs;
